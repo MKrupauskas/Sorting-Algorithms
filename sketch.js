44a349bb52d78376792327ed59a08b0f647637f1
Mykolas Krupauskas <m.krupauskas@gmail.com>
2017-06-01 22:30:12 +0300

    Added TODO

--- sketch.js
+++ sketch.js
@@ -1,9 +1,12 @@
+// TODO
+// Draw rect
+// Make a control bar on the top of the page
+// 
 var example = [30];
 
 function setup() {
 
 }
-
 function draw() {
   createCanvas(windowWidth, windowHeight);
   background(255, 50, 50);
