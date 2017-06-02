45360fe646c7fb8cbdac23624758717219984f49
Mykolas Krupauskas <m.krupauskas@gmail.com>
2017-06-02 22:06:49 +0300

    removed P5.js
    project is moving in a different direction

--- sketch.js
+++ sketch.js
@@ -1,12 +1,14 @@
 // TODO
 // Draw rect
 // Make a control bar on the top of the page
-// 
+//
 var example = [30];
 
 function setup() {
 
 }
+
+
 function draw() {
   createCanvas(windowWidth, windowHeight);
   background(255, 50, 50);
