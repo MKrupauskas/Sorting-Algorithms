f8726c3aaf4fcb282e336a2a5f608ad1f95c8d75
Mykolas Krupauskas <m.krupauskas@gmail.com>
2017-06-05 23:24:18 +0300

    Now loops through and creates an array with ordered numbers

--- script.js
+++ script.js
@@ -2,10 +2,13 @@
 // Draw rect
 // Make a control bar on the top of the page
 //
-let example = [30];
+let arayLength = 30;
 
 window.onload = function() {
-  
+  for (let array = [], i = 1; i < arrayLenght; i++) {
+    array[i] = i;
+  }
+  console.log(array)
 }
 
 
